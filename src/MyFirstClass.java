b1d78d215e0234a8dafccddd1c9e945b2b21c59a
Balraj Singh <balraj.singh@signitysolutions.in>
2021-02-15 14:38:00 +0530

    creation of New Branch

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -5,6 +5,8 @@ public class MyFirstClass {
 		System.out.println("My First hit Hub project");
 		
 		System.out.println("My First hit Hub project Pull Request" );
+		
+		System.out.println("My First hit Hub project New Branch Request" );
 
 	}
 
